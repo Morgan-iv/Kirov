250aabc54824a31d007523652723d9886e28c761
Morgan-iv <ivushkinalex@gmail.com>
2019-02-23 16:14:52 +0300

    first algo 0.1

--- Classes.hpp
+++ Classes.hpp
@@ -5,30 +5,39 @@
 
 
 enum BenchType{Saw,Plasma,Turning,Milling,Drill,Welding,Paint};
-struct Work{
+struct Work
+{
 	BenchType Type;
 	int Time;
 	int PrepareTime;
-	Work(BenchType type,int time,int prepare_time){
+	Work(BenchType type,int time,int prepare_time)
+	{
 		Type=type;
 		Time=time;
 		PrepareTime=prepare_time;
 	}
 };
 
-struct Detail{
+struct Detail
+{
 	std::string Name;
 	int Amount;
 	int Done;
+	int Num;
 	std::vector<Work> Sequence;
-	Detail(std::string name){
+	Detail(std::string name)
+	{
 		Name=name;
 	}
 };
 
-struct Bench{
+struct Bench
+{
+	size_t Remain;
+	size_t Current;
+	size_t Num;
 	BenchType Type;
-	std::vector<Detail> Queue;
+	std::list<size_t> Queue;
 };
 
  
